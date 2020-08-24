7ca949466f4daa5b98f079698b9531f511cff650
eiken7kyuu <11444818+eiken7kyuu@users.noreply.github.com>
2020-08-24 14:27:48 +0900

    小文字のurlを許可

--- ProtectedDiary/Startup.cs
+++ ProtectedDiary/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,11 +26,12 @@ namespace ProtectedDiary
         public void ConfigureServices(IServiceCollection services)
         {
             var builder = services.AddRazorPages();
-
             if (Env.IsDevelopment())
             {
                 builder.AddRazorRuntimeCompilation();
             }
+
+            services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
